b4b330fbed2094e43f744c77ce2f3c8ee2ea0b1b
Micaela Jawor <micaelajawor@yahoo.com>
2019-02-22 14:48:51 -0800

    update program.cs

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,21 +1,42 @@
-using System;
-using System.Collections.Generic;
-using WordCounter.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WordCounter
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Counter counter = new Counter();
-      Console.WriteLine("Write a word");
-      string userWord = Console.ReadLine();
-      Console.WriteLine("Write a sentence");
-      string userSentence = Console.ReadLine();
-      string[] sentenceArray = counter.SplitFunction(userSentence);
-      int result = counter.FindFunction(userWord, sentenceArray);
-      Console.WriteLine("There are " + result + " matches! Hurray...");
+      var host = new WebHostBuilder()
+          .UseKestrel()
+          .UseContentRoot(Directory.GetCurrentDirectory())
+          .UseIISIntegration()
+          .UseStartup<Startup>()
+          .Build();
+
+      host.Run();    
     }
   }
 }
+
+// using System;
+// using System.Collections.Generic;
+// using WordCounter.Models;
+//
+// namespace WordCounter
+// {
+//   public class Program
+//   {
+//     public static void Main()
+//     {
+//       Counter counter = new Counter();
+//       Console.WriteLine("Write a word");
+//       string userWord = Console.ReadLine();
+//       Console.WriteLine("Write a sentence");
+//       string userSentence = Console.ReadLine();
+//       string[] sentenceArray = counter.SplitFunction(userSentence);
+//       int result = counter.FindFunction(userWord, sentenceArray);
+//       Console.WriteLine("There are " + result + " matches! Hurray...");
+//     }
+//   }
+// }
