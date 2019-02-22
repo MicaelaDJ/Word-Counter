484da4063d262dd3be5d29209f7de103966495af
Micaela Jawor <micaelajawor@yahoo.com>
2019-02-22 10:06:34 -0800

    make basic wordcountercontrollertests

---
+++ WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordCounterControllerTest
+  {
+    [TestMethod]
+    public void Create_ReturnsCorrectActionType_RedirectToActionResult()
+    {
+      WordCounterController controller = new WordCounterController();
+      IActionResult view = controller.Create("There are 2 matches");
+      Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+    }
+
+    [TestMethod]
+    public void Create_RedirectsToCorrectAction_Index()
+    {
+      ItemsController controller = new ItemsController();
+      RedirectToActionResult actionResult = controller.Create("There are 2 matches") as RedirectToActionResrult;
+      string result = actionResult.ActionName;
+      Assert.AreEqual(result, "Index");
+    }
+  }
+}
